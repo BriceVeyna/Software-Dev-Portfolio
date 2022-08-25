f7d2ce7f2c72e62190c3fbd44dc07d8d14c09e4f
Brice Veyna <bricev99@gmail.com>
2022-08-25 08:59:57 -0700

    Add base resume sections

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,6 +4,29 @@ function Resume() {
     return (
         <div>
             <h2>Resume Page</h2>
+            <a href='/Brice-Veyna-Resume.pdf' download>Download Resume</a>
+            <h3>Front-end Proficiencies</h3>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>jQuery</li>
+                <li>Bootstrap</li>
+                <li>Foundation</li>
+                <li>React</li>
+                <li>responsive design</li>
+            </ul>
+
+            <h3>Back-end Proficiencies</h3>
+            <ul>
+                <li>APIs</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>MySQL, Sequelize</li>
+                <li>MongoDB, Mongoose</li>
+                <li>REST</li>
+                <li>GraphQL</li>
+            </ul>
         </div>
     );
 }
