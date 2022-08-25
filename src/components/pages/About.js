5a2a749e0c23789a3a5daa41ec2594e679f1edf2
Brice Veyna <bricev99@gmail.com>
2022-08-24 18:11:45 -0700

    Add base structure, bio

---
+++ src/components/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>
+                Hello and welcome to my portfolio! My name is Brice Veyna. I'm originally from Visalia, California and am currently living close to home in the Tulare countryside. My career journey into the world of software systems and technologies is kind of a long and meandering one.
+
+                From a young age I have worked with my mom, painting large public murals across the western US.
+
+                I then recieved my BS in Mechanical Engineering with a minor in Geography. Through the course of the program I was exposed to various machine interfacing and analysis software programs and languages. Additionally, the focus of my minor was Geographic Information Systems (GIS) and I worked as a student Research Assistant to my professor and created a prototype spatial-historical archive for the history of Fresno County.
+
+                Upon graduation I decided to further enhance my hands-on understanding of mechanical components and apprenticed/worked as CNC Machinist. While working there, I build a home hobby CNC router table as well and did several art projects with it. Through this I came to better understand machine interfacing software, as well as a proficient machinist and a better engineer.
+
+                I then accepted a position as a consulting Mechanical Design Engineer in the Siouxland region of South Dakota / Iowa.
+            </p>
+        </div>
+    );
+}
