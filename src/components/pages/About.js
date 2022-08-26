778aa8eac201da642c031eefa09d7acf2435a754
Brice Veyna <bricev99@gmail.com>
2022-08-25 19:25:50 -0700

    Add base styling to about section

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,20 +1,44 @@
 import React from 'react';
 
+const styles = {
+    portrait: {
+        float: 'left',
+        height: '450px',
+        margin: '0 25px 25px 0',
+        borderRadius: '10px',
+    },
+    bio: {
+        textIndent: '35px',
+    },
+};
+
 function About() {
     return (
-        <div>
-            <h2>About Page</h2>
-            <p>
-                Hello and welcome to my portfolio! My name is Brice Veyna. I'm originally from Visalia, California and am currently living close to home in the Tulare countryside. My career journey into the world of software systems and technologies is kind of a long and meandering one.
+        <div className='p-5 pt-0'>
+            <h2 className='mb-4 text-center'>A Bit About Myself</h2>
+            <img src='./assets/images/Portrait.png' alt='Brice Veyna Portrait' style={styles.portrait}></img>
+            <div style={styles.bio}>
+                <p>
+                    Hello and welcome to my portfolio! My name is Brice Veyna. I'm originally from Visalia, California and am currently living close to home in the Tulare countryside. My career journey into the world of software systems and technologies is kind of a long and meandering one.
+                </p>
 
-                From a young age I have worked with my mom, painting large public murals across the western US.
+                <p>
+                    From a young age I have worked with my mom, painting large public murals across the western US.
+                </p>
 
-                I then recieved my BS in Mechanical Engineering with a minor in Geography. Through the course of the program I was exposed to various machine interfacing and analysis software programs and languages. Additionally, the focus of my minor was Geographic Information Systems (GIS) and I worked as a student Research Assistant to my professor and created a prototype spatial-historical archive for the history of Fresno County.
+                <p>
+                    I then recieved my BS in Mechanical Engineering with a minor in Geography. Through the course of the program I was exposed to various machine interfacing and analysis software programs and languages. Additionally, the focus of my minor was Geographic Information Systems (GIS) and I worked as a student Research Assistant to my professor and created a prototype spatial-historical archive for the history of Fresno County.
+                </p>
 
-                Upon graduation I decided to further enhance my hands-on understanding of mechanical components and apprenticed/worked as CNC Machinist. While working there, I build a home hobby CNC router table as well and did several art projects with it. Through this I came to better understand machine interfacing software, as well as a proficient machinist and a better engineer.
+                <p>
+                    Upon graduation I decided to further enhance my hands-on understanding of mechanical components and apprenticed/worked as CNC Machinist. While working there, I build a home hobby CNC router table as well and did several art projects with it. Through this I came to better understand machine interfacing software, as well as a proficient machinist and a better engineer.
+                </p>
 
-                I then accepted a position as a consulting Mechanical Design Engineer in the Siouxland region of South Dakota / Iowa.
-            </p>
+                <p>
+                    I then accepted a position as a consulting Mechanical Design Engineer in the Siouxland region of South Dakota / Iowa.
+                </p>
+
+            </div>
         </div>
     );
 }
