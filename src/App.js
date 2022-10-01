ff0704e41a7a6d13f99d4f5bb7c49c265fb3e077
Brice Veyna <bricev99@gmail.com>
2022-10-01 10:28:22 -0700

    Updates

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
-import './App.css';
-
-const App = () => <PortfolioContainer />;
-
-export default App;
