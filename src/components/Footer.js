057c2bc333c002ac916ac369b984d8cc67e27745
Brice Veyna <bricev99@gmail.com>
2022-10-02 11:02:36 -0700

    Add links to profiles on other sites, change display style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,19 +2,30 @@ import React from 'react';
 
 const styles = {
     card: {
-        position: 'fixed',
-        bottom: 0,
-        left: 0,
+        position: 'relative',
+        bottom: '0',
         width: '100%',
-        background: "darkBlue",
+        background: "MidnightBlue",
         color: "antiqueWhite",
+        textAlign: 'center'
     },
+    link: {
+        filter: 'invert(100%) brightness(200%)'
+    },
+    links: {
+        filter: 'invert(10%)'
+    }
 };
 
 function Footer() {
     return (
-        <footer className='text-center p-3' style={styles.card}>
-            <h5>&copy; 2022 Brice Veyna</h5>
+        <footer className='p-3' style={styles.card}>
+            <div className='pb-4' style={styles.links}>
+                <a href={'https://github.com/BriceVeyna'} className='p-4'><img src='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg' alt='GitHub' height={50} style={styles.link}></img></a>
+                <a href={'https://www.linkedin.com/in/brice-veyna/'} className='p-4'><img src='https://upload.wikimedia.org/wikipedia/commons/7/76/Font_Awesome_5_brands_linkedin.svg' alt='LinkedIn' height={55} style={styles.link}></img></a>
+                <a href={'https://medium.com/@BriceVeyna'} className='p-4'><img src='https://upload.wikimedia.org/wikipedia/commons/e/ec/Medium_logo_Monogram.svg' alt='Medium' height={48} style={styles.link}></img></a>
+            </div>
+            <h6>&copy; 2022 Brice Veyna</h6>
         </footer>
     );
 }
